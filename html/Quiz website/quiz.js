bb30ff9678089b44d94a574f32bad6d5e310c46c
anushka-pande <2022bit042@sggs.ac.in>
2024-01-24 02:11:36 +0530

    quiz website updated

--- html/Quiz website/quiz.js
+++ html/Quiz website/quiz.js
@@ -5,15 +5,27 @@ function myFunction() {
   document.getElementById("mydropdown").classList.toggle("show");
 }
 
-window.onclick = function(event){
-  if(!event.target.matches('.dropbtn')){
+document.addEventListener("click", function (event) {
+  if (!event.target.matches('.dropbtn')) {
     var dropdown = document.getElementsByClassName("dropdown-content");
     var i;
-    for(i=0;i< dropdown.length; i++){
+    for (i = 0; i < dropdown.length; i++) {
       var openDropdown = dropdown[i];
-      if(openDropdown.classList.contains('show')){
+      if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
+});
+
+var modal = document.getElementById('id01');
+
+function openModal() {
+  modal.style.display = "block";
 }
+
+modal.addEventListener("click", function (event){
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
+});
