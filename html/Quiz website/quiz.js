03c2f6a33d347e0d1c1f5ffc7a9c577a41b57476
anushka-pande <2022bit042@sggs.ac.in>
2024-01-24 00:51:13 +0530

    modified website

--- html/Quiz website/quiz.js
+++ html/Quiz website/quiz.js
@@ -1,2 +1,19 @@
 const audio = new Audio();
 audio.src = "./mouse-click-153941.mp3";
+
+function myFunction() {
+  document.getElementById("mydropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+  if(!event.target.matches('.dropbtn')){
+    var dropdown = document.getElementsByClassName("dropdown-content");
+    var i;
+    for(i=0;i< dropdown.length; i++){
+      var openDropdown = dropdown[i];
+      if(openDropdown.classList.contains('show')){
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
