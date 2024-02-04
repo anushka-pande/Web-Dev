4bfe70f6a60358a510181b0092b5c6079cc2904c
anushka-pande <2022bit042@sggs.ac.in>
2024-02-04 16:44:04 +0530

    update website

--- html/Quiz website/rules.js
+++ html/Quiz website/rules.js
@@ -3,5 +3,6 @@ const audio = document.getElementById("myaudio");
 button.addEventListener("click", () => {
   audio.play();
   setTimeout(() => {
+    window.location.href = "quiz.html";
   }, audio.duration * 1000);
 });
